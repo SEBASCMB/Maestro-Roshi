f7a82175a28a8a813acf46c6d06c03a11a17714f
Sebastian Marquez <sebastiancmarquez1998@gmail.com>
2024-09-05 17:59:58 -0500

    Fix: Fixed js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,40 @@
-const navToggle = document.getElementById('nav-toggle');
-const navMenu   = document.getElementById('nav-menu');
-const navClose  = document.getElementById('nav-close');
+const navToggle = document.getElementById("nav-toggle");
+const navMenu   = document.getElementById("nav-menu");
+const navClose  = document.getElementById("nav-close");
 
 // Mostrar MENU
 if (navToggle) {
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu');
-  });
+	navToggle.addEventListener("click", () => {
+		navMenu.classList.add("show-menu");
+	});
 }
 
 // Ocultar MENU
 if (navClose) {
-  navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu');
-  });
+	navClose.addEventListener("click", () => {
+		navMenu.classList.remove("show-menu");
+	});
 }
 
 // Remover MENU de link
 
-const navLink = document.querySelectorAll(".nav__link")
-function linkAction(){
-  const navMenu = document.getElementById("nav-menu")
-  navMenu.classList.remove("show-menu")
+const navLink = document.querySelectorAll(".nav__link");
+function linkAction() {
+	const navMenu = document.getElementById("nav-menu");
+	navMenu.classList.remove("show-menu");
 }
 
-navLink.forEach(item => item.addEventListener("click",linkAction));
+for (const item of navLink) {
+	item.addEventListener("click", linkAction);
+}
 
 // Scroll Reveal
 
 const sr = ScrollReveal({
-  distance:"90px",
-  duration: 3000
-})
+	distance: "90px",
+	duration: 3000,
+});
 
-sr.reveal(`.home__data`,{origin:"top", delay: 400})
-sr.reveal(`.home__img`,{origin:"bottom", delay: 600})
-sr.reveal(`.home__footer`,{origin:"bottom", delay: 800})
+sr.reveal(".home__data", { origin: "top", delay: 400 });
+sr.reveal(".home__img", { origin: "bottom", delay: 600 });
+sr.reveal(".home__footer", { origin: "bottom", delay: 800 });
